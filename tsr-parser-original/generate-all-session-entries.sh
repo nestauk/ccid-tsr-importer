2f5c317f71dd53a91146bbacd2440bc3b6c60720
Lewis Westbury <lewis.westbury@nesta.org.uk>
2023-07-06 14:06:26 +0100

    gitignore output directory:

--- tsr-parser-original/generate-all-session-entries.sh
+++ tsr-parser-original/generate-all-session-entries.sh
@@ -4,11 +4,18 @@
 set -e 
 set -o pipefail
 
-BUCKET_NAME=collective-simulation-tsr-data-uploads
-SESSIONS_PATH=s3/$BUCKET_NAME/syndicateos-data/nesta/*/
+S3_BUCKET=collective-simulation-tsr-data-uploads
+SESSIONS_PATH=s3/$S3_BUCKET/syndicateos-data/nesta/*/
+
+# init directories
+mkdir -p s3/$S3_BUCKET
+mkdir -p working
+rm -r working/*
+mkdir -p working/files
+mkdir -p output
 
 # synchronise local data with bucket
-./sync-s3.sh -b $BUCKET_NAME
+./sync-s3.sh -b $S3_BUCKET
 
 for d in $SESSIONS_PATH ; do
     PATH_KEY=$(basename $d)
@@ -16,8 +23,8 @@ for d in $SESSIONS_PATH ; do
     echo "Preparing session: $d"
     echo
 
-    ./generate-session-entry.sh -s $PATH_KEY -b $BUCKET_NAME
+    ./generate-session-entry.sh -s $PATH_KEY -b $S3_BUCKET
     echo
 done
 
-./count-outputs -b $BUCKET_NAME
+./count-outputs -b $S3_BUCKET
