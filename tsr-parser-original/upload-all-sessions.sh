b10d7fe5265fc2affcd8578d69ac175bec1b1bbf
Lewis Westbury <lewis.westbury@nesta.org.uk>
2024-09-13 13:49:57 +0100

    Halt upload-all-sessions.sh before it creates difficulties

--- tsr-parser-original/upload-all-sessions.sh
+++ tsr-parser-original/upload-all-sessions.sh
@@ -6,6 +6,9 @@ set -o pipefail
 
 SESSION_TABLE=tsr-importer-app-SessionTable-O89MWVA1W5BQ
 
+echo "Are you sure you want to do this? The full set of sessions includes testing data that should not be included."
+exit 1
+
 echo "Uploading files..."
 for SESSION_FILE in output/*; do
     ./upload-session.sh -f $SESSION_FILE -t $SESSION_TABLE
